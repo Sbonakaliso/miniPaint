e7e386468c7877b17fe9ea2a4c6fea49841814d6
Sbonakaliso <kunenesbonakaliso@gmail.com>
2016-03-02 02:45:27 +0200

    Create mentor.php

---
+++ mentor.php
@@ -0,0 +1,17 @@
+<?php
+
+if($_POST["submit"]) {
+    $recipient="kunenesbonakaliso@gmail.com";
+    $subject="From Mentor Club Website";
+    $sender=$_POST["sender"];
+    $senderEmail=$_POST["senderEmail"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
+}
+
+?>
